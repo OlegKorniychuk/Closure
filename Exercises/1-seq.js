5452c72f67e95a1b52e8d7207a7ea363d5e8f7d8
OlegKorniychuk <kornoleg576@gmail.com>
2021-11-25 22:49:17 +0200

    What have I just coded?

--- Exercises/1-seq.js
+++ Exercises/1-seq.js
@@ -1,5 +1,9 @@
 'use strict';
 
-const seq = (f) => (g) => (x) => 0;
+const seq = (f) => (g) => (
+  typeof g === 'function' ? seq((x) => f(g(x))) : f(g)
+);
+
+console.dir(seq((x) => 2 * x)((x) => x + 5)((x) => x + 4)(3));
 
 module.exports = { seq };
--- Exercises/2-array.js
+++ Exercises/2-array.js
@@ -1,5 +1,19 @@
 'use strict';
 
-const array = () => null;
+const array = () => {
+  const a = [];
+  const getElement = (i) => a[i];
+  getElement.pop = () => a.pop();
+  getElement.push = function(data) {
+    a.push(data);
+  };
+  return getElement;
+};
+
+const Aboba = array();
+Aboba.push('a');
+Aboba.push('b');
+console.log(Aboba.pop(1));
+console.log(Aboba(0));
 
 module.exports = { array };
